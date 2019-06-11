e651f4c15ae8bd5f9f0734023cc53ef01afcd2fe
ZJ <zak@newalexandria.org>
2019-06-11 13:00:24 -0400

    added Workplace.com.   Re-added whatsapp

--- src/background.js
+++ src/background.js
@@ -9,10 +9,13 @@ const FACEBOOK_DOMAINS = [
   "fbcdn.net", "fbcdn.com", "fbsbx.com", "tfbnw.net",
   "facebook-web-clients.appspot.com", "fbcdn-profile-a.akamaihd.net", "fbsbx.com.online-metrix.net", "connect.facebook.net.edgekey.net",
 
-  "instagram.com",
-  "cdninstagram.com", "instagramstatic-a.akamaihd.net", "instagramstatic-a.akamaihd.net.edgesuite.net",
+  "instagram.com", "cdninstagram.com",
+  "instagramstatic-a.akamaihd.net", "instagramstatic-a.akamaihd.net.edgesuite.net", 
 
   "messenger.com", "m.me", "messengerdevelopers.com",
+  "whatsapp.com", "whatsapp.net",
+
+  "workplace.com",
 
   "atdmt.com",
 
